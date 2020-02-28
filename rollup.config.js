2a706b8070ba920d9691389f9e7f54d44c110e08
Marco Arthur <arthurpbs@gmail.com>
2020-02-28 16:30:23 -0300

    Add webpack plugin and set for rollup
    Mojolicious::Plugin::Webpack support, so we can start devel mode with
    help of webpack mojo command.

--- rollup.config.js
+++ rollup.config.js
@@ -3,69 +3,83 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
+import html from 'rollup-plugin-bundle-html';
+import postcss from 'rollup-plugin-postcss';
 
 const production = !process.env.ROLLUP_WATCH;
 
+const dest = process.env.WEBPACK_OUT_DIR || 'public/asset';
+function outPath(fn) {
+    const filename = production ? fn : fn.replace(/\[hash\]/, 'development');
+    return [dest, filename].join('/');
+}
+
 export default {
-	input: 'src/main.js',
-	output: {
-		sourcemap: true,
-		format: 'iife',
-		name: 'app',
-		file: 'public/build/bundle.js'
-	},
-	plugins: [
-		svelte({
-			// enable run-time checks when not in production
-			dev: !production,
-			// we'll extract any component CSS out into
-			// a separate file — better for performance
-			css: css => {
-				css.write('public/build/bundle.css');
-			}
-		}),
+    input: 'src/main.js',
+    output: {
+	sourcemap: true,
+	    format: 'iife',
+	    name: 'app',
+	    file: outPath('app.[hash].js')
+    },
+    plugins: [
+	svelte({
+	    // enable run-time checks when not in production
+	    dev: !production,
+	    // we'll extract any component CSS out into
+	    // a separate file — better for performance
+	    css: css => {
+		css.write(outPath('app.[hash].css'));
+	    }
+	}),
 
-		// If you have external dependencies installed from
-		// npm, you'll most likely need these plugins. In
-		// some cases you'll need additional configuration —
-		// consult the documentation for details:
-		// https://github.com/rollup/plugins/tree/master/packages/commonjs
-		resolve({
-			browser: true,
-			dedupe: importee => importee === 'svelte' || importee.startsWith('svelte/')
-		}),
-		commonjs(),
+	// If you have external dependencies installed from
+	// npm, you'll most likely need these plugins. In
+	// some cases you'll need additional configuration —
+	// consult the documentation for details:
+	// https://github.com/rollup/plugins/tree/master/packages/commonjs
+	resolve({
+	    browser: true,
+	    dedupe: importee => importee === 'svelte' || importee.startsWith('svelte/')
+	}),
+	commonjs(),
 
-		// In dev mode, call `npm run start` once
-		// the bundle has been generated
-		!production && serve(),
+	// Needed by M::P::WebPack so it can include generated files using
+	// helper ``asset'' see:
+	// https://metacpan.org/pod/Mojolicious::Plugin::Webpack#asset
+	html({
+	    dest,
+	    filename: 'webpack.' + (production ? 'production' : 'development') + '.html',
+	    inject: 'head',
+	    template: '<html><head></head><body></body></html>',
+	}),
+	// Watch the `public` directory and refresh the
+	// browser on changes when not in production
+	!production && livereload('public'),
 
-		// Watch the `public` directory and refresh the
-		// browser on changes when not in production
-		!production && livereload('public'),
-
-		// If we're building for production (npm run build
-		// instead of npm run dev), minify
-		production && terser()
-	],
-	watch: {
-		clearScreen: false
-	}
+	// If we're building for production (npm run build
+	// instead of npm run dev), minify
+	production && terser(),
+	postcss({extract: true, plugins: [] }),
+    ],
+    watch: {
+	clearScreen: false
+    }
 };
 
 function serve() {
-	let started = false;
+    let started = false;
 
-	return {
-		writeBundle() {
-			if (!started) {
-				started = true;
+    return {
+	writeBundle() {
+	    if (!started) {
+		started = true;
 
-				require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-					stdio: ['ignore', 'inherit', 'inherit'],
-					shell: true
-				});
-			}
-		}
-	};
+		require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
+		    stdio: ['ignore', 'inherit', 'inherit'],
+		    shell: true
+		});
+	    }
+	}
+    };
 }
